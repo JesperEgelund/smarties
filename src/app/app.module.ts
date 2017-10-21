8b9e7b2ef04ee5120cff98f735445f48eaef0565
je@jesperegelund.dk <je@jesperegelund.dk>
2017-10-21 21:50:50 +0800

    Converted to Scss + Added Mat Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+
+import {
+  MatToolbarModule,
+  MatSidenavModule
+
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      MatToolbarModule,
+      MatSidenavModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
